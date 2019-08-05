23ed777cd887e3da51daaf49786b9aea15c80e36
Alex <agordon1123@gmail.com>
2019-08-05 18:59:49 -0400

    project complete

--- src/App.js
+++ src/App.js
@@ -17,18 +17,17 @@ const todoData = [
   }
 ];
 
+// * 5 * Created my React.Component App
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // * 6 * Created my constructor and initialized state with mockData above
   constructor() {
     super();
     this.state = {
       items: todoData
     }
-    console.log(this.state)
   }
 
+  // * 13 * Created toggleItem to map through props and check if the item.id matches current state's id. If yes, it inverses the value of completed (which will work with our ternary in ToDo.js). If else, it returns the item.
   toggleItem = id => {
     this.setState({
       items: this.state.items.map(i => {
@@ -45,6 +44,7 @@ class App extends React.Component {
     })
   }
 
+  // * 7 * Created addItem fn to initialize newItem added from form input and set to State
   addItem = itemName => {
     const newItem = {
       task: itemName,
@@ -56,6 +56,7 @@ class App extends React.Component {
     })
   }
 
+  // * 16 * Created clearItems fn to filter out and remove any items within state where completed is true
   clearItems = () => {
     this.setState({
       items: this.state.items.filter(i => !i.completed)
@@ -67,11 +68,13 @@ class App extends React.Component {
       <div className='app'>
         <h1>Goals</h1>
         <p>Stay motivated and focused with this interactive to-do list built using React classes.</p>
+        {/* * 14 * Called ToDoList and passed state and the toggleItems fn */}
         <ToDoList 
           items={this.state.items} 
           toggleItem={this.toggleItem} 
           clearItems={this.clearItems} 
         />
+        {/* * 8 * Called ToDoForm and pass addItem */}
         <ToDoForm addItem={this.addItem} />
       </div>
     );
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
+// * 1 * Create form that will take user input and render to a list
 class ToDoForm extends React.Component {
+    // * 2 * Created my constructor and initialized this.state to an object with a key for each item
     constructor() {
         super();
         this.state = {
@@ -8,11 +10,13 @@ class ToDoForm extends React.Component {
         }
     }
 
+    // * 9 * Created onSubmit handler to add newItem value to this.state
     submitItem = e => {
         e.preventDefault();
         this.props.addItem(this.state.item);
     }
 
+    // * 3 * Created my changeHandler to setState to e(input).target.value
     handleChanges = e => {
         this.setState({
             [e.target.name]: e.target.value
@@ -21,6 +25,7 @@ class ToDoForm extends React.Component {
 
     render() {
         return (
+            // * 4 * Created my form, input, and button
             <form className='form' onSubmit={this.submitItem}>
                 <input 
                     type='text'
